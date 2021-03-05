5bfff035656bfe78be532cccad923dbb2ae2f168
RakibRahman <md.rr.talukder@gmail.com>
2021-03-05 22:41:29 +0600

    innerHTML updated

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const users = document.querySelector("#user");
 const userNext = document.querySelector("#nxt-user");
 
 const getRandomUsers = async () => {
-  const fetchUsers = await fetch("https://randomuser.me/api/?results=2");
+  const fetchUsers = await fetch("https://randomuser.me/api/");
   const randomUser = await fetchUsers.json();
   randomUser.results.forEach((user) => {
-    const { name, gender, email, login } = user;
+    const { picture, name, gender, email, login } = user;
 
-    users.innerHTML = `First Name: ${name.first} </br> Last Name: ${name.last} </br> Gender: ${gender} </br> Email: ${email} </br> Password: ${login.password}`;
+    users.innerHTML = `<img src="${picture.large}" width="200" height="200" style="border-radius:50%"> </br> First Name: ${name.first} </br> Last Name: ${name.last} </br> Gender: ${gender} </br> Email: ${email} </br> Password: ${login.password}`;
   });
   return randomUser;
 };
